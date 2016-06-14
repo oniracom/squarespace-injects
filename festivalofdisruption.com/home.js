49d1ff5c0fc0a5028f02f838a0c2c5939f74ada7
Sean Campos <sean@oniracom.com>
2016-06-14 09:52:09 -0700

    add document onready for dlf

--- festivalofdisruption.com/home.js
+++ festivalofdisruption.com/home.js
@@ -1,8 +1,11 @@
-var buttonWrapper = Y.Node.create('<div></div>');
-buttonWrapper.append(Y.Node.create('<a href="/tickets" class="sqs-editable-button">get tickets</a>'));
-buttonWrapper.append(Y.Node.create('<a href="/festival-information" class="sqs-editable-button">what to expect</a>'));
 
-Y.Node.one('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper').insert(buttonWrapper, Y.Node.all('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper .parallax-item').shift());
+Y.on('domready', function () {
+	var buttonWrapper = Y.Node.create('<div></div>');
+	buttonWrapper.append(Y.Node.create('<a href="/tickets" class="sqs-editable-button">get tickets</a>'));
+	buttonWrapper.append(Y.Node.create('<a href="/festival-information" class="sqs-editable-button">what to expect</a>'));
+
+	Y.Node.one('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper').insert(buttonWrapper, Y.Node.all('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper .parallax-item').shift());
+});
 
 // Y.Node.all('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper .parallax-item').shift().insert(
 /*
