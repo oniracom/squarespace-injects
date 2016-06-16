1f72327ac289fa9d18b5a9ba9a45e179189217a9
Sean Campos <sean@oniracom.com>
2016-06-16 10:56:35 -0700

    change festival button label

--- festivalofdisruption.com/home.js
+++ festivalofdisruption.com/home.js
@@ -2,7 +2,7 @@
 Y.on('domready', function () {
 	var buttonWrapper = Y.Node.create('<div id="home-top-buttons"></div>');
 	buttonWrapper.append(Y.Node.create('<a href="/tickets" class="sqs-editable-button">get tickets</a>'));
-	buttonWrapper.append(Y.Node.create('<a href="/festival-information" class="sqs-editable-button">the festival</a>'));
+	buttonWrapper.append(Y.Node.create('<a href="/festival-information" class="sqs-editable-button">dive in</a>'));
 
 	Y.Node.one('#collection-5746db4c9f7266e08f9b2bd0 #content-wrapper').insert(buttonWrapper, Y.Node.one('#page-5746db531d07c0426d6b2944'));
 });
